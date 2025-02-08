27e4d6dab55238e33bdc2444f48ee14b4ee711ed
Sakura Ryoko <sakura@ryoko.soy>
2025-02-08 12:45:40 -0500

    disable debug log for config saved

--- src/main/java/fi/dy/masa/minihud/config/Configs.java
+++ src/main/java/fi/dy/masa/minihud/config/Configs.java
@@ -377,7 +377,7 @@ public class Configs implements IConfigHandler
                     }
                 }
 
-                MiniHUD.debugLog("loadFromFile(): Successfully loaded config file '{}'.", configFile.toAbsolutePath());
+                //MiniHUD.debugLog("loadFromFile(): Successfully loaded config file '{}'.", configFile.toAbsolutePath());
             }
             else
             {
@@ -396,7 +396,7 @@ public class Configs implements IConfigHandler
         if (!Files.exists(dir))
         {
             FileUtils.createDirectoriesIfMissing(dir);
-            //ItemScroller.debugLog("saveToFile(): Creating directory '{}'.", dir.toAbsolutePath());
+            //MiniHUD.debugLog("saveToFile(): Creating directory '{}'.", dir.toAbsolutePath());
         }
 
         if (Files.isDirectory(dir))
