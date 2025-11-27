a7c49192bad706271e39ad4fe37f5b8469fbbbf9
Sakura Ryoko <sakura@ryoko.soy>
2025-11-27 15:32:34 -0500

    rename some of the vanilla debug renderer options to more closely match vanilla's names.

--- src/main/java/fi/dy/masa/minihud/hotkeys/KeyCallbacks.java
+++ src/main/java/fi/dy/masa/minihud/hotkeys/KeyCallbacks.java
@@ -77,7 +77,7 @@ public class KeyCallbacks
         RendererToggle.SHAPE_RENDERER.setValueChangeCallback(RendererCallbacks::onShapeRendererToggled);
 
         RendererToggle.DEBUG_CHUNK_BORDER.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
-	    RendererToggle.DEBUG_OCTREEE.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
+	    RendererToggle.DEBUG_CHUNK_SECTION_OCTREEE.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
 	    RendererToggle.DEBUG_WATER.setValueChangeCallback(DebugInfoUtils::toggleDebugDataConfig);
 	    RendererToggle.DEBUG_HEIGHTMAP.setValueChangeCallback(DebugInfoUtils::toggleDebugDataConfig);
 	    RendererToggle.DEBUG_COLLISION_BOXES.setValueChangeCallback(DebugInfoUtils::toggleDebugDataConfig);
@@ -89,8 +89,8 @@ public class KeyCallbacks
 	    RendererToggle.DEBUG_CHUNK_LOADING.setValueChangeCallback(DebugInfoUtils::toggleDebugDataConfig);
 	    RendererToggle.DEBUG_SKYLIGHT_SECTIONS.setValueChangeCallback(DebugInfoUtils::toggleDebugDataConfig);
 	    RendererToggle.DEBUG_ENTITY_HITBOXES.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
-        RendererToggle.DEBUG_CHUNK_INFO.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
-        RendererToggle.DEBUG_CHUNK_OCCLUSION.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
+        RendererToggle.DEBUG_CHUNK_SECTION_PATHS.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
+        RendererToggle.DEBUG_CHUNK_SECTION_VISIBILITY.setValueChangeCallback(DebugInfoUtils::toggleDebugRenderer);
 
 	    RendererToggle.DEBUG_DATA_MAIN_TOGGLE.setValueChangeCallback(RendererCallbacks::onDebugServiceToggled);
 	    RendererToggle.DEBUG_PATH_FINDING.setValueChangeCallback(DebugInfoUtils::toggleDebugDataConfig);
--- src/main/java/fi/dy/masa/minihud/util/DebugInfoUtils.java
+++ src/main/java/fi/dy/masa/minihud/util/DebugInfoUtils.java
@@ -26,10 +26,14 @@ public class DebugInfoUtils
                 debugWarn(enabled ? "debug.chunk_boundaries.on" : "debug.chunk_boundaries.off");
             }
         }
-        else if (config == RendererToggle.DEBUG_OCTREEE)
+        else if (config == RendererToggle.DEBUG_ENTITY_HITBOXES)
         {
-			toggleDebugHud(DebugHudEntries.CHUNK_SECTION_OCTREE, config, true);
+	        toggleDebugHud(DebugHudEntries.ENTITY_HITBOXES, config, true);
         }
+//        else if (config == RendererToggle.DEBUG_BLOCK_OUTLINE)
+//        {
+//	        toggleDebugHud(DebugHudEntries.VISUALIZE_SOLID_FACES, config, true);
+//        }
 //        else if (config == RendererToggle.DEBUG_WATER)
 //        {
 //	        toggleDebugHud(DebugHudEntries.VISUALIZE_WATER_LEVELS, config, true);
@@ -54,10 +58,6 @@ public class DebugInfoUtils
 //        {
 //	        toggleDebugHud(DebugHudEntries.VISUALIZE_SKY_LIGHT_LEVELS, config, true);
 //        }
-//        else if (config == RendererToggle.DEBUG_BLOCK_OUTLINE)
-//        {
-//	        toggleDebugHud(DebugHudEntries.VISUALIZE_SOLID_FACES, config, true);
-//        }
 //        else if (config == RendererToggle.DEBUG_CHUNK_LOADING)
 //        {
 //	        toggleDebugHud(DebugHudEntries.VISUALIZE_CHUNKS_ON_SERVER, config, true);
@@ -66,15 +66,15 @@ public class DebugInfoUtils
 //        {
 //	        toggleDebugHud(DebugHudEntries.VISUALIZE_SKY_LIGHT_SECTIONS, config, true);
 //        }
-        else if (config == RendererToggle.DEBUG_ENTITY_HITBOXES)
+        else if (config == RendererToggle.DEBUG_CHUNK_SECTION_OCTREEE)
         {
-	        toggleDebugHud(DebugHudEntries.ENTITY_HITBOXES, config, true);
+	        toggleDebugHud(DebugHudEntries.CHUNK_SECTION_OCTREE, config, true);
         }
-        else if (config == RendererToggle.DEBUG_CHUNK_INFO)
+        else if (config == RendererToggle.DEBUG_CHUNK_SECTION_PATHS)
         {
 	        toggleDebugHud(DebugHudEntries.CHUNK_SECTION_PATHS, config, true);
         }
-        else if (config == RendererToggle.DEBUG_CHUNK_OCCLUSION)
+        else if (config == RendererToggle.DEBUG_CHUNK_SECTION_VISIBILITY)
         {
 	        toggleDebugHud(DebugHudEntries.CHUNK_SECTION_VISIBILITY, config, true);
         }
@@ -126,10 +126,10 @@ public class DebugInfoUtils
 		if (inst != null)
 		{
 			RendererToggle.DEBUG_CHUNK_BORDER.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.CHUNK_BORDERS));
-			RendererToggle.DEBUG_OCTREEE.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.CHUNK_SECTION_OCTREE));
+			RendererToggle.DEBUG_CHUNK_SECTION_OCTREEE.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.CHUNK_SECTION_OCTREE));
 			RendererToggle.DEBUG_ENTITY_HITBOXES.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.ENTITY_HITBOXES));
-			RendererToggle.DEBUG_CHUNK_INFO.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.CHUNK_SECTION_PATHS));
-			RendererToggle.DEBUG_CHUNK_OCCLUSION.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.CHUNK_SECTION_VISIBILITY));
+			RendererToggle.DEBUG_CHUNK_SECTION_PATHS.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.CHUNK_SECTION_PATHS));
+			RendererToggle.DEBUG_CHUNK_SECTION_VISIBILITY.setBooleanValueNoCallback(inst.isEntryVisible(DebugHudEntries.CHUNK_SECTION_VISIBILITY));
 		}
 	}
 }
